f04339762a1a2f7a39ebd4e1a85f005814c59a75
Mageza <admageza@yahoo.com>
2019-03-09 10:17:38 +0000

    init

--- app/models/admission.rb
+++ app/models/admission.rb
@@ -1,4 +1,13 @@
 class Admission < ApplicationRecord
      mount_uploader :image, ImageUploader
      belongs_to :user, :optional => true
+     validates :image, presence: true
+     validates :FirstName, presence: true, length: { minimum: 3 }
+     validates :PlaceOfBirth, presence: true, length: { minimum: 3 }
+     validates :DateOfBirth, presence: true
+     validates :level, presence: true, length: { maximum: 5 }
+     validates :FatherName, presence: true, length: { minimum: 3 }
+     validates :FatherPhone, presence: true, length: { maximum: 15 }
+     validates :MotherName, presence: true, length: { minimum: 3 }
+     validates :MotherPhone, presence: true, length: { maximum: 15 }
 end
