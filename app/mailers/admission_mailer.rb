a2648cb521621aea87485342e6c1e70225a5144d
Mageza <admageza@yahoo.com>
2019-03-31 14:48:09 +0000

    init

--- app/mailers/admission_mailer.rb
+++ app/mailers/admission_mailer.rb
@@ -1,6 +1,5 @@
 class AdmissionMailer < ApplicationMailer
-  default from: "admin@saintdominic.com"
-
+ 
   def admission_mail(emailu)
    
     mail to: emailu, subject: 'Congratulation, Your Child is registered'
